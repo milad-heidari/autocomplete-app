5402625105388cc8592e599629ff960696ed5eba
milad heidari <milad.heidari.dev@gmail.com>
2021-09-23 12:41:38 +0330

    complete autocomplete app

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import Elements from "../Elements/ElementsExport";
+import data from "../data.json";
+function MainLayout() {
+  const [trueItems, setTrueItems] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    let matches = [];
+    if (searchText.length > 0) {
+      data.states.filter((state) => {
+        const regexp = new RegExp(`^${searchText}`, "gi");
+        if (state.name.match(regexp) || state.abbr.match(regexp)) {
+          matches.push(state);
+        }
+      });
+    }
+    console.log("matches", matches);
+    setTrueItems(matches);
+  }, [searchText]);
+
+  const search = document.getElementById("search");
+
+  if (search) {
+    search.addEventListener("input", () => {
+      setSearchText(search.value);
+    });
+  }
+
+  const renderItems = trueItems.map((item) => {
+    return (
+      <Elements.Div className="elements-form__match-list-item">
+        <Elements.P
+          className="elements-form__match-list-p"
+          text={`${item.name} (${item.abbr}) ${item.capital}  `}
+        />
+      </Elements.Div>
+    );
+  });
+
+  return (
+    <>
+      <Elements.Form className="elements-form" onSubmit={() => {}}>
+        <Elements.P className="elements-form_p" text="Search For State" />
+        <Elements.Input
+          className="elements-form_input"
+          placeholder="Enter State Name or abbreviation..."
+          id="search"
+        />
+        <Elements.Div className="elements-form__match-list">
+          {trueItems.length > 0 ? renderItems : null}
+        </Elements.Div>
+      </Elements.Form>
+    </>
+  );
+}
+
+export default MainLayout;
